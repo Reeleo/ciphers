ac5ecc856d20a00f2e832f3bddc21213bd8e3c45
Reeleo <168227248+Reeleo@users.noreply.github.com>
2025-09-22 18:48:30 +0100

    Update ceasar.py

--- ceasar.py
+++ ceasar.py
@@ -1,6 +1,36 @@
 
 
 
+def stringToArray(string):
+    array = []
+    for char in range(len(string)):
+        array.append(string[char])
+    return array
+
+def arrayToString(array):
+    string = ""
+    for item in range(len(array)):
+        string += array[item]
+    return string
+
+
+def setUp(cipher):
+    cipher = cipher.lower()
+    cipher = stringToArray(cipher)
+    counter = 0
+    valid = False
+    while not valid:
+        ascii = ord(cipher[counter])
+        if ascii < 97 or ascii > 123:
+            cipher.pop(counter)
+        else:
+            counter += 1
+        if counter == len(cipher):
+            valid = True
+    cipher = arrayToString(cipher)
+    return cipher
+
+
 
 def dictionary(txt):
     diff = 0
@@ -35,8 +65,7 @@ def ceasar(cipher):
     array = []
     diffs = []
     cipher.lower()
-    for char in range(len(cipher)):
-        array.append(cipher[char])
+    array = stringToArray(cipher)
     for shift in range(26):
         for i in range(len(array)):
             if array[i] != " ":
@@ -44,15 +73,16 @@ def ceasar(cipher):
                 if ascii+1 >= 123:
                     ascii = 96
                 array[i] = chr(ascii+1)
-        #print(array,dictionary(array))
-        diffs.append([array,dictionary(array),shift])
+        #print(array,dictionary(array),shift,"\n")
+        shifted = []
+        for j in range(len(array)):
+            shifted.append(array[j])
+        diffs.append([shifted,dictionary(array),shift])
     pointer = 0
     for j in range(len(diffs)):
         if diffs[j][1] > diffs[pointer][1]:
             pointer = j
-        elif diffs[j][1] == diffs[pointer][1]:
-            print(diffs[pointer])
-            pointer = j
+    diffs[pointer][0] = arrayToString(diffs[pointer][0])
     return diffs[pointer]
 
 
@@ -66,7 +96,11 @@ def ceasar(cipher):
 
 
 
+a = "GUR ANGVBANY NEPUVIRF VA XRJ PBAGNVA N GUBHFNAQ LRNEF BS UVFGBEL. QBPHZRAGF BS FGNGR QNGVAT ONPX BIRE N GUBHFNAQ LRNEF NER PNERSHYYL CERFREIRQ NAQ FGHQVRQ OL UVFGBEVNAF SEBZ NEBHAQ GUR JBEYQ – SVYRF GUNG NER BSGRA FRAFVGVIR NAQ ZNEXRQ JVGU N ERYRNFR QNGR JUVPU PBHYQ OR NF YBAT NF BAR UHAQERQ LRNEF SBE CNEGVPHYNEYL FRAFVGVIR VASBEZNGVBA. ZHPU YRFF JRYY XABJA VF GUR FUNQBJ NEPUVIR, QRRC VAFVQR GUR UBZR BS OEVGVFU PBQROERNXVAT JUVPU PBAGNVAF SVYRF GUNG ZVTUG ARIRE OR ERYRNFRQ. GURL PNEEL GUR ZNEX QGN, JUVPU QRABGRF QRYRTNGR GB NEPUNRBYBTL, GUR GRNZ ERFCBAFVOYR SBE GURZ OHG GUR NEPUVIVFGF WBXR GUNG VG FGBBQ SBE QBA\’G GRYY NALBAR. GURFR SVYRF PBAGNVA FRPERGF GUNG, NYBAT JVGU GURVE PVCURE XRLF, JRER YBFG YBAT NTB. AB-BAR XABJF JUNG GERNFHERF GURL ZVTUG PBAGNVA. VG VF LBHE WBO GB OERNX GUR PVCUREF NAQ GB SVAQ BHG!"
+b = "EDCT OEEW VAKUE DZK U OUXX BAHJ JOA ZEO KACLYEZJH NAG QAL JA KECUBTEG. JTEQ DGE JDWEZ NGAY JTE DGCTUMEH TEGE DJ IAHH CEZJGDX DZK GEXDJE JA EMEZJH ACCLGGUZS AMEG AZE TLZKGEK DZK NUNJQ QEDGH DSA. U TDME D NEEXUZS JTDJ JTEQ YUSTJ HJUXX IE GEXEMDZJ!!! QALG JDHW UH JA KECGQBJ EDCT AN JTE KACLYEZJH DZK LBXADK QALG HAXLJUAZH AZ JTE CTDXXEZSE BDSEH. UN QAL YDWE D YUHJDWE QAL OUXX SEJ HAYE NEEKIDCW DZK CDZ JGQ DSDUZ. QAL OUXX SEJ D HCAGE NAG EDCT DJJEYBJ DZK OE OUXX ILUXK D XEDKEGIADGK LHUZS JTAHE HCAGEH. UN QAL DGE UZ D JEDY, JTEZ AZXQ JTE JEDY CDBJDUZ CDZ LBXADK HLIYUHHUAZH, HA YDWE HLGE QAL OAGW OUJT JTEY JA SEJ QALG DZHOEGH HLIYUJJEK. UZ QALG DCCALZJ BDSEH QAL OUXX NUZK D GECAGK AN QALG HLIYUHHUAZH, JASEJTEG OUJT D CABQ AN D CEGJUNUCDJE GECAGKUZS QALG BGASGEHH. UN QAL ODZJ QAL CDZ DXXAO QALG JEDCTEG JA NAXXAO QALG BGASGEHH IQ SEJJUZS JTE JEDY CDBJDUZ JA EZJEG D HBECUDX BUZ ZLYIEG (JTE JEDCTEG\’H YAZUJAGUZS BUZ) UZJA JTE JEDY KEJDUXH AZ JTEUG DCCALZJ BDSE. UN QAL TDME DZQ FLEHJUAZH JTEZ TEDK AMEG JA JTE NAGLY OTEGE QAL OUXX NUZK XAJH AN NGUEZKXQ DKMUCE! SAAK XLCW, TDGGQ"
 
-ciphertxt = input("cipher text: ")
-print(ceasar(ciphertxt))
+a = setUp(a)
+b = setUp(b)
+
+print(ceasar(a))
 
