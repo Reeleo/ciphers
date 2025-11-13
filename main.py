4bb1ab2648cb71057975b337648b636760ace696
Reeleo <168227248+Reeleo@users.noreply.github.com>
2025-11-13 16:00:04 +0000

    trying to do part b

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ import solve
 import freq
  
 
-fiveBkey = {"a":"a", "b":"b", "c":"c", "d":"r", "e":"e", "f":"f", "g":"g",
-        "h":"h", "i":"d", "j":"j", "k":"k", "l":"y", "m":"m", "n":"n",
-        "o":"e", "p":"p", "q":"q", "r":"r", "s":"s", "t":"t", "u":"u",
-        "v":"v", "w":"m", "x":"x", "y":"y", "z":"z"} 
-
 fiveBkey = {"a":"a", "b":"b", "c":"c", "d":"d", "e":"e", "f":"f", "g":"g",
         "h":"h", "i":"i", "j":"j", "k":"k", "l":"l", "m":"m", "n":"n",
         "o":"o", "p":"p", "q":"q", "r":"r", "s":"s", "t":"t", "u":"u",
         "v":"v", "w":"w", "x":"x", "y":"y", "z":"z"} 
 
-#mydear
-fiveBkey = "qnvqao"
+fiveBkey = {"a":"a", "b":"j", "c":"p", "d":"i", "e":"n", "f":"t", "g":"l",
+        "h":"b", "i":"h", "j":"c", "k":"x", "l":"m", "m":"q", "n":"v",
+        "o":"e", "p":"w", "q":"r", "r":"r", "s":"z", "t":"f", "u":"k",
+        "v":"d", "w":"g", "x":"s", "y":"o", "z":"y"} 
 
-#dearest
-fiveBkey = "htsdape"
+# #mydear
+# fiveBkey = "qnvqao"
+
+# #dearest
+# fiveBkey = "htsdape"
 
 
 #print(freq.freqLetters(ciphers.fiveB))
-print(solve.vigenere(ciphers.fiveB,fiveBkey))
+print(solve.sSubstitution(ciphers.fiveB,fiveBkey))
 
 
+print(sorted(freq.FREQ.items(), key=lambda x:x[1], reverse=True))
 
 
 
